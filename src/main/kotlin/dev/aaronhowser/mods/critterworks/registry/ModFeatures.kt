6bb6e127728a7405120b54c693a2a15532738e42
aaron <aaronhowser1@gmail.com>
2026-09-08 14:17:48 -0500

    helper method

--- src/main/kotlin/dev/aaronhowser/mods/critterworks/registry/ModFeatures.kt
+++ src/main/kotlin/dev/aaronhowser/mods/critterworks/registry/ModFeatures.kt
@@ -7,6 +7,7 @@ import net.minecraft.core.registries.Registries
 import net.minecraft.world.level.levelgen.feature.Feature
 import net.neoforged.neoforge.registries.DeferredHolder
 import net.neoforged.neoforge.registries.DeferredRegister
+import java.util.function.Supplier
 
 object ModFeatures {
 
@@ -14,8 +15,12 @@ object ModFeatures {
 		DeferredRegister.create(Registries.FEATURE, Critterworks.MOD_ID)
 
 	val SCOOCHWORM_APPLE: DeferredHolder<Feature<*>, ScoochwormAppleFeature> =
-		FEATURE_REGISTRY.register("scoochworm_apple", ::ScoochwormAppleFeature)
-
+		register("scoochworm_apple", ::ScoochwormAppleFeature)
 	val HOPPING_SPIDER_NEST: DeferredHolder<Feature<*>, HoppingSpiderNestFeature> =
-		FEATURE_REGISTRY.register("hopping_spider_nest", ::HoppingSpiderNestFeature)
+		register("hopping_spider_nest", ::HoppingSpiderNestFeature)
+
+	private fun <T : Feature<*>> register(string: String, function: Supplier<T>): DeferredHolder<Feature<*>, T> {
+		return FEATURE_REGISTRY.register(string, function)
+	}
+
 }
